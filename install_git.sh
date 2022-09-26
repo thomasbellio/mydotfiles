e4b3e1da9cd5ca01e04ff78c83fc959bf5c43a2b
Thomas Bell <thomas.bell@deepsee.ai>
2022-09-26 08:46:24 -0600

    updated the prepare-commit-msg hook to be executable

--- install_git.sh
+++ install_git.sh
@@ -1,4 +1,6 @@
 #/bin/bash
+echo "initializing git templates directory..." &&
 mkdir -p $HOME/.git-templates/hooks/ &&
+echo "creating symlink to templates directory..." &&
 ln  $(pwd)/git/templates/hooks/prepare-commit-msg $HOME/.git-templates/hooks/prepare-commit-msg &&
 git config --global init.templatedir $HOME/.git-templates 
