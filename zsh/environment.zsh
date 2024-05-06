a505676fbc63140006c4c1978135a77ef65b721d
Thomas Bell <thomas.bell@deepsee.ai>
2024-05-06 15:19:37 -0600

    conditional sourcing of rust env

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -21,7 +21,10 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 export PAGER="less"
-source $HOME/.cargo/env
+if [[ -f $HOME/.cargo/env ]]; then
+    source $HOME/.cargo/env
+fi
+
 ## Git Config
 git config --global core.editor nvim
 
