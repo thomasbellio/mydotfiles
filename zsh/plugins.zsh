2f04d8c1501ab2d9a2e74967249f89d89b0684d6
thomas-mac <thomas@thomas-macs-MacBook-Pro.local>
2024-06-21 15:13:29 -0600

    added krew and an alias for getting secrets

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,4 +1,5 @@
 source ${(q-)HOME}/shell-plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source ${(q-)HOME}/shell-plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 
 
