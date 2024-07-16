6f4057b278bb1fc0be8fe98d7d509f8f3a7ad513
Thomas Bell <thomas.bell@chargezoom.com>
2024-07-16 09:51:47 -0600

    added composer source

--- zsh/php.zsh
+++ zsh/php.zsh
@@ -1,6 +1,4 @@
- export PHPBREW_SET_PROMPT=1
-# export PHPBREW_RC_ENABLE=1
+export PHPBREW_SET_PROMPT=1
 
 [[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc
-
-
+[[ -e ~/.config/composer/vendor/bin/ ]] && export PATH="$PATH:$HOME/.config/composer/vendor/bin" 
