923854749f9d5a603f4f3ffc2471f798a6b3b4bb
Thomas A Bell <git@thomasbell.io>
2025-05-11 13:40:51 -0600

    added logic to create the history file

--- zsh/core.zsh
+++ zsh/core.zsh
@@ -1,6 +1,13 @@
 # Lines configured by zsh-newuser-install
 _feature_debug "Sourcing zsh/core.zsh"
+
 export HISTFILE=~/history/histfile
+
+# Check if the directory for HISTFILE exists, if not, create it
+if [ ! -d "$(dirname "$HISTFILE")" ]; then
+    mkdir -p "$(dirname "$HISTFILE")"
+    chmod 700 "$(dirname "$HISTFILE")"
+fi
 export HISTSIZE=10000
 export SAVEHIST=10000
 unsetopt beep
