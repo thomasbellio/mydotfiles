b6d8fa20a79728beecfef5bf9f6531dbe5d41999
Thomas Bell <thomas@thomasbell.io>
2025-05-12 15:33:15 -0600

    moved all modules into directories

--- zsh/modules/php.zsh
+++
@@ -1,4 +0,0 @@
-export PHPBREW_SET_PROMPT=1
-
-[[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc
-[[ -e ~/.config/composer/vendor/bin/ ]] && export PATH="$PATH:$HOME/.config/composer/vendor/bin" 
---
+++ zsh/modules/php/php-brew.zsh
@@ -0,0 +1,4 @@
+export PHPBREW_SET_PROMPT=1
+
+[[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc
+[[ -e ~/.config/composer/vendor/bin/ ]] && export PATH="$PATH:$HOME/.config/composer/vendor/bin"
