ac8e092a8aa8243076705d21b3879ceaa6ee3b46
Thomas Bell <thomas@thomasbell.io>
2025-05-23 15:23:09 -0600

    added check for systemd units

--- install-systemd-units.sh
+++ install-systemd-units.sh
@@ -8,5 +8,8 @@ fi
 echo "Creating symlink for ssh-agent.service."
 ln -s $(pwd)/systemd/user/ssh-agent.service "$XDG_CONFIG_HOME/systemd/user/ssh-agent.service"
 
-systemctl --user enable ssh-agent.service
-systemctl --user start ssh-agent.service
+if ! systemctl --user is-enabled --quiet ssh-agent.service; then
+    systemctl --user enable --now ssh-agent.service
+else
+    systemctl --user daemon-reload
+fi
