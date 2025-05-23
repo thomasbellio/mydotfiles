f70052f79e512e19285f4a2d5db5995e785275ea
Thomas Bell <thomas@thomasbell.io>
2025-05-23 15:25:41 -0600

    added doc comment

--- zsh/modules/arch_core/ssh.zsh
+++ zsh/modules/arch_core/ssh.zsh
@@ -1,4 +1,6 @@
 export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 if [ ! -f ~/.ssh/config ] || ! grep -q 'AddKeysToAgent' ~/.ssh/config; then
+    # This will ensure identities are added automatically for new sesssions
+    # https://man.openbsd.org/ssh_config#AddKeysToAgent
     echo 'AddKeysToAgent  yes' >> ~/.ssh/config
 fi
