418a856c3387768e583927a2eefb5c2fc3cbda11
Thomas Bell <thomas@thomasbell.io>
2025-08-28 12:17:49 -0600

    updated the aliases to have a dev-shell command

--- zsh/modules/arch_core/aliases.zsh
+++ zsh/modules/arch_core/aliases.zsh
@@ -3,7 +3,7 @@ alias zv="nvim ~/.zshrc"
 alias ls="ls -alhG --color=auto"
 alias copy="wl-copy"
 alias paste="wl-paste"
-shell() {
+dev_shell() {
     local machine_name="$1"
     sudo machinectl shell thomas-devel@"$machine_name" /usr/bin/env WAYLAND_DISPLAY="$WAYLAND_DISPLAY" /usr/bin/zsh -l
 }
