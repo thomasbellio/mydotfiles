7838db87dce6a780e50feb8ade33d3301af8bd98
Thomas Bell <thomas@thomasbell.io>
2025-09-03 14:20:04 -0600

    updated android environment

--- zsh/modules/android/environment.zsh
+++ zsh/modules/android/environment.zsh
@@ -1,2 +1,2 @@
-export ANDROID_HOME="$HOME/.android-sdk"
-export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/emulator"
+export ANDROID_SDK_HOME="$HOME/.android-sdk"
+export PATH="$PATH:$ANDROID_SDK_HOME/cmdline-tools/latest/bin:$ANDROID_SDK_HOME/emulator"
