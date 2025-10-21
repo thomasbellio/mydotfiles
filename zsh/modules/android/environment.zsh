c305920548dd170d1251519809d60b5236f7f3b6
Thomas Bell <thomas@thomasbell.io>
2025-10-21 11:43:40 -0600

    updated android module

--- zsh/modules/android/environment.zsh
+++ zsh/modules/android/environment.zsh
@@ -1,6 +1,6 @@
 export ANDROID_SDK_HOME="$HOME/.android-sdk"
+export ANDROID_SDK_HOME="$HOME/.android-sdk"
 export PATH="$PATH:$ANDROID_SDK_HOME/cmdline-tools/bin:$ANDROID_SDK_HOME/emulator"
-
 if [[ -n "$ANDROID_BUILD_TOOLS_VERSION" ]]; then
     export PATH="$PATH:$ANDROID_SDK_HOME/build-tools/$ANDROID_BUILD_TOOLS_VERSION"
 fi
