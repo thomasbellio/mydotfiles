9b8f3e889336b642ca59e7e78b0bc18699b2d6bf
Thomas Bell <thomas@thomasbell.io>
2025-11-23 08:16:57 -0700

    added python module and profile

--- shell/zsh/modules/python/environment.zsh
+++ shell/zsh/modules/python/environment.zsh
@@ -1,4 +1,6 @@
-if [[ "$OSTYPE" == "darwin"* ]]; then
+if which virtualenvwrapper.sh &>/dev/null; then
+    export WORKON_HOME=$HOME/.virtualenvs
+    export PROJECT_HOME=$HOME/code
     source virtualenvwrapper.sh
 fi
 
